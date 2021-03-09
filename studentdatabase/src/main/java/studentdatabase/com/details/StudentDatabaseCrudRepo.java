81bd9f11731a0aefc35c9ac0bb564f3de5004ce4
vishale17 <74190341+vishale17@users.noreply.github.com>
2021-03-09 15:54:39 +0530

    Delete studentdatabase directory

--- studentdatabase/src/main/java/studentdatabase/com/details/StudentDatabaseCrudRepo.java
+++
@@ -1,7 +0,0 @@
-package studentdatabase.com.details;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentDatabaseCrudRepo extends CrudRepository<StudentDatabaseDetails,Integer>{
-
-}
